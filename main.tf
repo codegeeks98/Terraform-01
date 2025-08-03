53e691964f8c983a65461c0189cc1d9e9a09c0e7
codegeeks98 <codegeeks98@gmail.com>
2025-08-03 18:06:19 +0530

    commit 01

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+
+
+# plan-execute
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-in28mins-adit"
+
+    versioning {
+        enabled = true 
+    }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_aditya"
+}
+
+
+
+# STATES
+# DESIRED - KNOWN - ACTUAL
