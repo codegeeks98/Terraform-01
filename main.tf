0ca2c98a6fc15f5b76dc4a2d16dabe1a75b7e9e0
Aditya Singh <codegeeks98@gmail.com>
2025-08-03 18:08:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,9 @@ provider "aws" {
 
 # plan-execute
 
+
+# code to create an AWS S3 bucket
+
 resource "aws_s3_bucket" "my_s3_bucket" {
     bucket = "my-s3-bucket-in28mins-adit"
 
@@ -24,6 +27,8 @@ resource "aws_s3_bucket" "my_s3_bucket" {
         enabled = true 
     }
 }
+
+# code to create a new IAM user
 
 resource "aws_iam_user" "my_iam_user" {
     name = "my_iam_user_aditya"
